b37a3fe97a8074e034dc0ba27d1dc2195d40f58a
Luka Doric <luka.doric@uns.ac.rs>
2020-11-15 17:19:31 +0100

    Initialization of project

---
+++ Pharmacy-Backend/src/test/java/rs/ac/uns/ftn/isa/pharmacy/demo/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.isa.pharmacy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
