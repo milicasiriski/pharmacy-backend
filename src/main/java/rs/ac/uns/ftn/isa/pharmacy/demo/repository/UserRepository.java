367d94365136c7be303ae91d5ffd531c07c88bfc
lukaDoric <luka.doric@uns.ac.rs>
2021-01-20 19:36:22 +0100

    [#32] Minor fix

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/UserRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/UserRepository.java
@@ -17,6 +17,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @Query(value = "SELECT id, email, password, pharmacy_id, enabled, last_password_reset_date, user_type, name, surname FROM pharmacy_user WHERE user_type = 'DERMATOLOGIST'", nativeQuery=true)
     List<Dermatologist> getAllDermatologists();
 
-    @Query(value = "SELECT pharmacy_user.id, email, password, pharmacy.id, enabled, last_password_reset_date, user_type, pharmacy_user.name, surname FROM pharmacy_user JOIN pharmacy_dermatologist ON pharmacy_user.id = pharmacy_dermatologist.dermatologist_id JOIN pharmacy ON pharmacy.id = pharmacy_dermatologist.pharmacy_id WHERE pharmacy.name = :pharmacyName and user_type = 'DERMATOLOGIST'", nativeQuery=true)
+    @Query(value = "SELECT DISTINCT pharmacy_user.id, email, password, pharmacy.id, enabled, last_password_reset_date, user_type, pharmacy_user.name, surname FROM pharmacy_user JOIN pharmacy_dermatologist ON pharmacy_user.id = pharmacy_dermatologist.dermatologist_id JOIN pharmacy ON pharmacy.id = pharmacy_dermatologist.pharmacy_id WHERE pharmacy.name = :pharmacyName and user_type = 'DERMATOLOGIST'", nativeQuery=true)
     List<Dermatologist> getDermatologistsByPharmacy(@Param("pharmacyName") String pharmacyName);
 }
