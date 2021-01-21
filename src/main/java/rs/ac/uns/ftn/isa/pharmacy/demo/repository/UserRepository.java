0826776c6e91b72b43112b0b4031f2fedc7c45f7
lukaDoric <luka.doric@uns.ac.rs>
2021-01-21 13:58:42 +0100

    [#33] Fixing some paths, user is able to filter pharmacists

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/UserRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/UserRepository.java
@@ -15,15 +15,15 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByEmail(String email);
 
-    @Query(value = "SELECT id, email, password, enabled, last_password_reset_date, user_type, name, surname FROM pharmacy_user WHERE user_type = 'DERMATOLOGIST'", nativeQuery=true)
+    @Query(value = "SELECT id, email, password, enabled, last_password_reset_date, user_type, name, surname FROM pharmacy_user WHERE user_type = 'DERMATOLOGIST'", nativeQuery = true)
     List<Dermatologist> getAllDermatologists();
 
-    @Query(value = "SELECT DISTINCT pharmacy_user.id, email, password, pharmacy.id, enabled, last_password_reset_date, user_type, pharmacy_user.name, surname FROM pharmacy_user JOIN pharmacy_dermatologist ON pharmacy_user.id = pharmacy_dermatologist.dermatologist_id JOIN pharmacy ON pharmacy.id = pharmacy_dermatologist.pharmacy_id WHERE pharmacy.name = :pharmacyName and user_type = 'DERMATOLOGIST'", nativeQuery=true)
+    @Query(value = "SELECT DISTINCT pharmacy_user.id, email, password, pharmacy.id, enabled, last_password_reset_date, user_type, pharmacy_user.name, surname FROM pharmacy_user JOIN pharmacy_dermatologist ON pharmacy_user.id = pharmacy_dermatologist.dermatologist_id JOIN pharmacy ON pharmacy.id = pharmacy_dermatologist.pharmacy_id WHERE pharmacy.name = :pharmacyName and user_type = 'DERMATOLOGIST'", nativeQuery = true)
     List<Dermatologist> getDermatologistsByPharmacy(@Param("pharmacyName") String pharmacyName);
 
-    @Query(value = "SELECT id, email, password, enabled, last_password_reset_date, user_type, name, surname FROM pharmacy_user WHERE user_type = 'PHARMACIST'", nativeQuery=true)
+    @Query(value = "SELECT id, email, password, enabled, last_password_reset_date, user_type, name, surname FROM pharmacy_user WHERE user_type = 'PHARMACIST'", nativeQuery = true)
     List<Pharmacist> getAllPharmacists();
 
-    @Query(value = "SELECT DISTINCT pharmacy_user.id, email, password, pharmacy.id, enabled, last_password_reset_date, user_type, pharmacy_user.name, surname FROM pharmacy_user JOIN pharmacy_pharmacist ON pharmacy_user.id = pharmacy_pharmacist.pharmacist_id JOIN pharmacy ON pharmacy.id = pharmacy_pharmacist.pharmacy_id WHERE pharmacy.name = :pharmacyName and user_type = 'PHARMACIST'", nativeQuery=true)
+    @Query(value = "SELECT DISTINCT pharmacy_user.id, email, password, pharmacy.id, enabled, last_password_reset_date, user_type, pharmacy_user.name, surname FROM pharmacy_user JOIN pharmacy_pharmacists ON pharmacy_user.id = pharmacy_pharmacists.pharmacist_id JOIN pharmacy ON pharmacy.id = pharmacy_pharmacists.pharmacy_id WHERE pharmacy.name = :pharmacyName and user_type = 'PHARMACIST'", nativeQuery = true)
     List<Pharmacist> getPharmacistByPharmacy(@Param("pharmacyName") String pharmacyName);
 }
