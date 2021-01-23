bb326709535714ef2dc4fdce7a5c759b350cc5ef
lukaDoric <luka.doric@uns.ac.rs>
2021-01-23 20:57:11 +0100

    [#38] Get all vacation requests from dermatologists.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/impl/VacationServiceImpl.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/impl/VacationServiceImpl.java
@@ -4,35 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.VacationTimeRequestDermatologist;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.VacationTimeRequestPharmacist;
-import rs.ac.uns.ftn.isa.pharmacy.demo.repository.VacationRepository;
+import rs.ac.uns.ftn.isa.pharmacy.demo.repository.DermatologistVacationRepository;
+import rs.ac.uns.ftn.isa.pharmacy.demo.repository.PharmacistVacationRepository;
 import rs.ac.uns.ftn.isa.pharmacy.demo.service.VacationService;
-
-import java.util.List;
 
 @Service
 public class VacationServiceImpl implements VacationService {
 
-    private final VacationRepository vacationRepository;
+    private final PharmacistVacationRepository pharmacistVacationRepository;
+    private final DermatologistVacationRepository dermatologistVacationRepository;
 
     @Autowired
-    public VacationServiceImpl(VacationRepository vacationRepository) {
-        this.vacationRepository = vacationRepository;
+    public VacationServiceImpl(PharmacistVacationRepository pharmacistVacationRepository, DermatologistVacationRepository dermatologistVacationRepository) {
+        this.pharmacistVacationRepository = pharmacistVacationRepository;
+        this.dermatologistVacationRepository = dermatologistVacationRepository;
     }
 
-    @Override
     public Iterable<VacationTimeRequestPharmacist> getAllPharmacistsVacation() {
-
-     //   List<VacationTimeRequestPharmacist> vacationList = vacationRepository.getAllPharmacistsVacation();
-        return vacationRepository.findAll();
+        return pharmacistVacationRepository.findAll();
     }
 
-    @Override
-    public List<VacationTimeRequestDermatologist> getAllDermatologistsVacation() {
-        return null;
+    public Iterable<VacationTimeRequestDermatologist> getAllDermatologistsVacation() {
+        return dermatologistVacationRepository.findAll();
     }
-
-//    @Override
-//    public List<VacationTimeRequestDermatologist> getAllDermatologistsVacation() {
-//        return vacationRepository.getAllDermatologistVacation();
-//    }
 }
