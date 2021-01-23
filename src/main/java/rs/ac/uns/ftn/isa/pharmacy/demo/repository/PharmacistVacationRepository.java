5795f423dd6724e3dbc8aa428e49515c19807fe8
lukaDoric <luka.doric@uns.ac.rs>
2021-01-23 20:57:11 +0100

    [#38] Get all vacation requests from dermatologists.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/PharmacistVacationRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/PharmacistVacationRepository.java
@@ -3,5 +3,5 @@ package rs.ac.uns.ftn.isa.pharmacy.demo.repository;
 import org.springframework.data.repository.CrudRepository;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.VacationTimeRequestPharmacist;
 
-public interface VacationRepository extends CrudRepository<VacationTimeRequestPharmacist, Long> {
+public interface PharmacistVacationRepository extends CrudRepository<VacationTimeRequestPharmacist, Long> {
 }
--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/VacationService.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/VacationService.java
@@ -3,9 +3,7 @@ package rs.ac.uns.ftn.isa.pharmacy.demo.service;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.VacationTimeRequestDermatologist;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.VacationTimeRequestPharmacist;
 
-import java.util.List;
-
 public interface VacationService {
     Iterable<VacationTimeRequestPharmacist> getAllPharmacistsVacation();
-    List<VacationTimeRequestDermatologist> getAllDermatologistsVacation();
+    Iterable<VacationTimeRequestDermatologist> getAllDermatologistsVacation();
 }
