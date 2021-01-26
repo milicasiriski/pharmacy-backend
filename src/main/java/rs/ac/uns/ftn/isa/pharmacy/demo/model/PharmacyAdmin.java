2cfbb540a3d17526ed5386b20dbbe9bd0a0cd869
lukaDoric <luka.doric@uns.ac.rs>
2021-01-26 23:51:16 +0100

    [#29] Fixes due to sonar recommendations.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/model/PharmacyAdmin.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/model/PharmacyAdmin.java
@@ -2,6 +2,7 @@ package rs.ac.uns.ftn.isa.pharmacy.demo.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @DiscriminatorValue("PHARMACY_ADMIN")
@@ -33,6 +34,21 @@ public class PharmacyAdmin extends User implements Serializable {
 
     public void setPharmacy(Pharmacy pharmacy) {
         this.pharmacy = pharmacy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PharmacyAdmin that = (PharmacyAdmin) o;
+        return Objects.equals(administrationRole, that.administrationRole) &&
+                Objects.equals(pharmacy, that.pharmacy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), administrationRole, pharmacy);
     }
 
     @Override
