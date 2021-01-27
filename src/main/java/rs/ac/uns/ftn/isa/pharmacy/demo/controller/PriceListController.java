163daf281c6d87d152d2105527d6b5c726c3a49a
lukaDoric <luka.doric@uns.ac.rs>
2021-01-27 11:02:40 +0100

    [#29] Removing unused imports.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/controller/PriceListController.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/controller/PriceListController.java
@@ -5,22 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import rs.ac.uns.ftn.isa.pharmacy.demo.exceptions.PharmacyDoesNotContainMedicineException;
-import rs.ac.uns.ftn.isa.pharmacy.demo.model.*;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.dto.PriceListItemDto;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.dto.PriceListItemResponseDto;
-import rs.ac.uns.ftn.isa.pharmacy.demo.service.PharmacyService;
 import rs.ac.uns.ftn.isa.pharmacy.demo.service.PriceListService;
 
 import javax.persistence.EntityNotFoundException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Controller
