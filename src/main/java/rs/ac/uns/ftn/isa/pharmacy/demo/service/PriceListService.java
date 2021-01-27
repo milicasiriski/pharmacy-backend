4069ffac63c67bec54d3e146d3a0bbf9a1f37ec6
lukaDoric <luka.doric@uns.ac.rs>
2021-01-27 11:00:09 +0100

    [#29] Minor refactor.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/PriceListService.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/PriceListService.java
@@ -1,7 +1,11 @@
 package rs.ac.uns.ftn.isa.pharmacy.demo.service;
 
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.dto.PriceListItemDto;
+import rs.ac.uns.ftn.isa.pharmacy.demo.model.dto.PriceListItemResponseDto;
+
+import java.util.List;
 
 public interface PriceListService {
     void updatePriceList(PriceListItemDto priceListItemDto);
+    List<PriceListItemResponseDto> collectCurrentMedicinePrices();
 }
