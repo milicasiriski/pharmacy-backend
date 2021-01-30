d44513a06d6fd378ff3b032b065224c90071398b
lukaDoric <luka.doric@uns.ac.rs>
2021-01-30 22:31:58 +0100

    [#24] Added admin email to created order.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/impl/OrderServiceImpl.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/impl/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package rs.ac.uns.ftn.isa.pharmacy.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.Medicine;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.Order;
+import rs.ac.uns.ftn.isa.pharmacy.demo.model.PharmacyAdmin;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.dto.OrderDto;
 import rs.ac.uns.ftn.isa.pharmacy.demo.repository.MedicineRepository;
 import rs.ac.uns.ftn.isa.pharmacy.demo.repository.OrderRepository;
@@ -31,7 +33,7 @@ public class OrderServiceImpl implements OrderService {
         HashMap<Medicine, Integer> medicineAmount = convertMedicineOrderDtoToMap(orderDto);
         Calendar deadline = Calendar.getInstance();
         deadline.setTime(orderDto.getDeadline());
-        Order order = new Order(medicineAmount, deadline);
+        Order order = new Order(medicineAmount, deadline, ((PharmacyAdmin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
         orderRepository.save(order);
     }
 
