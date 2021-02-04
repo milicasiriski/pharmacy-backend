70dda182bf58e2bd31133474a0fe55374a0c8333
Milica Siriski <milica.siriski998@gmail.com>
2021-02-04 19:52:02 +0100

    [#16] Pharmacy search results can be sorted by price and rating (ascending and descending).

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/controller/PatientPharmacistExamController.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/controller/PatientPharmacistExamController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rs.ac.uns.ftn.isa.pharmacy.demo.model.Pharmacy;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.dto.GetPharmaciesForPharmacistExamResponse;
 import rs.ac.uns.ftn.isa.pharmacy.demo.service.PharmacistExamSchedulingService;
+import rs.ac.uns.ftn.isa.pharmacy.demo.util.PharmacySortType;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -29,14 +29,29 @@ public class PatientPharmacistExamController {
     @GetMapping("/pharmacies/{dateTime}")
     public ResponseEntity<Iterable<GetPharmaciesForPharmacistExamResponse>> getPharmaciesWithAvailablePharmacists(@PathVariable("dateTime") Date dateTime) {
         try {
-            List<GetPharmaciesForPharmacistExamResponse> response = new ArrayList<>();
-            pharmacistExamSchedulingService.getPharmaciesWithAvailableAppointments(dateTime).forEach(pharmacy ->
-                    response.add(new GetPharmaciesForPharmacistExamResponse(pharmacy))
-            );
+            Iterable<GetPharmaciesForPharmacistExamResponse> response = getPharmacies(dateTime, PharmacySortType.NONE);
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
-            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @PreAuthorize("hasRole('ROLE_PATIENT')") // NOSONAR the focus of this project is not on web security
+    @GetMapping("/pharmacies/{dateTime}/{sort}")
+    public ResponseEntity<Iterable<GetPharmaciesForPharmacistExamResponse>> getPharmaciesWithAvailablePharmacists(@PathVariable("dateTime") Date dateTime, @PathVariable("sort") PharmacySortType sort) {
+        try {
+            Iterable<GetPharmaciesForPharmacistExamResponse> response = getPharmacies(dateTime, sort);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private Iterable<GetPharmaciesForPharmacistExamResponse> getPharmacies(Date dateTime, PharmacySortType sort) {
+        List<GetPharmaciesForPharmacistExamResponse> result = new ArrayList<>();
+        pharmacistExamSchedulingService.getPharmaciesWithAvailableAppointments(dateTime, sort).forEach(pharmacy ->
+                result.add(new GetPharmaciesForPharmacistExamResponse(pharmacy))
+        );
+        return result;
     }
 }
