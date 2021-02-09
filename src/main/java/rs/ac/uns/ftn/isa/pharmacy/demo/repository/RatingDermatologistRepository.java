2f79e4d26b310460b8235bc9d8bc49d81d902a76
Milica Siriski <milica.siriski998@gmail.com>
2021-02-09 22:28:07 +0100

    [#41] User can only give one rating to each entity.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/RatingDermatologistRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/RatingDermatologistRepository.java
@@ -1,7 +1,15 @@
 package rs.ac.uns.ftn.isa.pharmacy.demo.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.RatingDermatologist;
 
+import java.util.Optional;
+
 public interface RatingDermatologistRepository extends CrudRepository<RatingDermatologist, Long> {
+    @Query(value = "SELECT rd.id, rd.dermatologist_id, r.rating, r.patient_id\n" +
+            "\tFROM rating_dermatologist AS rd, rating AS r WHERE rd.id = r.id\n" +
+            "\tAND r.patient_id = :patientId AND rd.dermatologist_id = :dermatologistId", nativeQuery = true)
+    Optional<RatingDermatologist> findByPatientIdAndDermatologistId(@Param("patientId") long patientId, @Param("dermatologistId") long dermatologistId);
 }
--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/RatingPharmacistRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/repository/RatingPharmacistRepository.java
@@ -1,7 +1,15 @@
 package rs.ac.uns.ftn.isa.pharmacy.demo.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.RatingPharmacist;
 
+import java.util.Optional;
+
 public interface RatingPharmacistRepository extends CrudRepository<RatingPharmacist, Long> {
+    @Query(value = "SELECT rp.id, rp.pharmacist_id, r.rating, r.patient_id\n" +
+            "\tFROM rating_pharmacist AS rp, rating AS r WHERE rp.id = r.id\n" +
+            "\tAND r.patient_id = :patientId AND rp.pharmacist_id = :pharmacistId", nativeQuery = true)
+    Optional<RatingPharmacist> findByPatientIdAndPharmacistId(@Param("patientId") long patientId, @Param("pharmacistId") long pharmacistId);
 }
