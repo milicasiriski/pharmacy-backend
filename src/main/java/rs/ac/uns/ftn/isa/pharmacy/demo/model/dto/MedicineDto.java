c280e51e6554f40c94abb5b7959acb950b369f34
Milica Siriski <milica.siriski998@gmail.com>
2021-02-10 02:53:54 +0100

    [#41] More sonar fixes.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/model/dto/MedicineDto.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/model/dto/MedicineDto.java
@@ -1,11 +1,9 @@
 package rs.ac.uns.ftn.isa.pharmacy.demo.model.dto;
 
-import rs.ac.uns.ftn.isa.pharmacy.demo.model.Medicine;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.enums.MedicineForm;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.enums.MedicineType;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
