4fa3053572d953c2619c3c2f88ad9135f2c992a4
Milica Siriski <milica.siriski998@gmail.com>
2021-02-11 19:59:29 +0100

    Removed redundancies.

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/config/WebConfig.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/config/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:8081",
-                "http://localhost:8082",
                 "http://team6-isa-front.herokuapp.com",
                 "https://team6-isa-front.herokuapp.com");
     }
