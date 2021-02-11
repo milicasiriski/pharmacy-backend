cfbe5ea3b3c1e16fcc7216caf54bd099ee633e0c
Milica Siriski <milica.siriski998@gmail.com>
2021-02-11 18:52:45 +0100

    Web config and CORS Filter

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/config/CORSFilter.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/config/CORSFilter.java
@@ -17,7 +17,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        response.setHeader("Access-Control-Allow-Origin", "*"); // NOSONAR
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "access_token, authorization, content-type");
