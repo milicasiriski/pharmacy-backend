67b6a1be3e2422859bcf45911848f6ff601bff9a
lukaDoric <luka.doric@uns.ac.rs>
2021-02-12 12:26:30 +0100

    Heroku fix

--- src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/impl/CustomUserDetailsService.java
+++ src/main/java/rs/ac/uns/ftn/isa/pharmacy/demo/service/impl/CustomUserDetailsService.java
@@ -1,10 +1,8 @@
 package rs.ac.uns.ftn.isa.pharmacy.demo.service.impl;
 
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.isa.pharmacy.demo.model.User;
 import rs.ac.uns.ftn.isa.pharmacy.demo.repository.UserRepository;
@@ -13,13 +11,9 @@ import rs.ac.uns.ftn.isa.pharmacy.demo.repository.UserRepository;
 public class CustomUserDetailsService implements UserDetailsService {
 
 	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	private final AuthenticationManager authenticationManager;
 
-	public CustomUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager){
+	public CustomUserDetailsService(UserRepository userRepository){
 		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.authenticationManager = authenticationManager;
 	}
 
 	@Override
